e364e2f88456bc9d58e974e16abe405bae116fbb
JOAO PAULO THEODORO SAMPAIO <95037249+joaopaulosampaio2@users.noreply.github.com>
2021-12-23 16:35:10 -0300

    Arquivos
    Adicionando arquivos

---
+++ JoaoPauloTheodoro_Projeto/JavaScript/JavaScript.js
@@ -0,0 +1,8 @@
+function Envio() {
+
+    var nome = document.getElementById("nomeid");
+    if (nome.value != "") {
+        alert('Muito Obrigado sr(a) ' + nome.value + ' sua mensagem foi enviada com sucesso');
+    }
+
+}

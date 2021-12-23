4834ab448304cb7d9d4fb1aafe7bfa72de9fbddb
JOAO PAULO THEODORO SAMPAIO <95037249+joaopaulosampaio2@users.noreply.github.com>
2021-12-23 16:40:47 -0300

    errei o repositório

--- JoaoPauloTheodoro_Projeto/JavaScript/JavaScript.js
+++
@@ -1,8 +0,0 @@
-function Envio() {
-
-    var nome = document.getElementById("nomeid");
-    if (nome.value != "") {
-        alert('Muito Obrigado sr(a) ' + nome.value + ' sua mensagem foi enviada com sucesso');
-    }
-
-}
